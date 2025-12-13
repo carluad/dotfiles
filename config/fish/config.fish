3c49204f7a5ee9aa3af82198b26c44d71684e33b
splyffer <splyffer@proton.me>
2025-12-13 06:36:01 -0300

    migrate to Chimera Linux

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -U fish_greeting
 
+    if test -f /usr/bin/helix
+        alias hx=helix
+    end
+
     starship init fish | source
     zoxide init fish | source
 end
