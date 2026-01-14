0d581b3ce22930c9e10c52170dc262cea699638c
carluad <carluad@proton.me>
2026-01-14 14:26:49 -0300

    Update

--- misc/setup-gnome-gsettings.sh
+++ misc/setup-gnome-gsettings.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' && echo "dark mode..."
 gsettings set org.gnome.desktop.interface accent-color 'teal' && echo "accent teal..."
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-teal' && echo "icon Adwaita-teal..."
 gsettings set org.gnome.desktop.interface cursor-theme "'Noir'" && echo "cursor Noir..."
 gsettings set org.gnome.desktop.wm.preferences button-layout "'appmenu'" && echo "Not close button on window"
-gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' && echo "GTK Theme Adwita-dark"
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark' && echo "GTK Theme Adwaita-dark"
